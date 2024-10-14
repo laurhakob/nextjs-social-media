e641c195dfed36510efa77c8e7cf268437c226de
laurhakob <laurhakob@gmail.com>
2024-10-14 19:25:58 +0400

    creating posts

--- nextjs-social-media-app/src/app/(main)/page.tsx
+++ nextjs-social-media-app/src/app/(main)/page.tsx
@@ -1,9 +1,11 @@
-import Image from "next/image";
+import PostEditor from "@/components/posts/editor/PostEditor";
 
 export default function Home() {
   return (
    <main className="h-[200vh] w-full bg-red-50">
-      <div className="w-full">Front page</div>
+      <div className="w-full">
+        <PostEditor />
+      </div>
    </main>
   );
 }
