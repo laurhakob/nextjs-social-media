ddd2c6ff593a14adaf6a325936a0d2fa726f4dc3
laurhakob <laurhakob@gmail.com>
2024-10-16 21:57:57 +0400

    react query introduction

--- nextjs-social-media-app/src/app/(main)/page.tsx
+++ nextjs-social-media-app/src/app/(main)/page.tsx
@@ -3,20 +3,22 @@ import Post from "@/components/posts/Post";
 import TrendsSidebar from "@/components/TrendsSidebar";
 import prisma from "@/lib/prisma";
 import { postDataInclude } from "@/lib/types";
+import ForYouFeed from "./ForYouFeed";
 
 export default async function Home() {
-  const posts = await prisma.post.findMany({
-    include: postDataInclude,
-    orderBy: { createdAt: "desc" },
-  });
+  // const posts = await prisma.post.findMany({
+  //   include: postDataInclude,
+  //   orderBy: { createdAt: "desc" },
+  // });
 
   return (
     <main className="flex w-full min-w-0 gap-5">
       <div className="w-full min-w-0 space-y-5">
         <PostEditor />
-        {posts.map((post) => (
+        <ForYouFeed />
+        {/* {posts.map((post) => (
           <Post key={post.id} post={post} />
-        ))}
+        ))} */}
       </div>
       <TrendsSidebar />
     </main>
--- nextjs-social-media-app/src/app/layout.tsx
+++ nextjs-social-media-app/src/app/layout.tsx
@@ -1,8 +1,9 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
-import { ThemeProvider } from "next-themes"
+import { ThemeProvider } from "next-themes";
 import { Toaster } from "@/components/ui/toaster";
+import ReactQueryProvider from "./ReactQueryProvider";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -29,17 +30,18 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="system"
-          enableSystem
-          disableTransitionOnChange
-        >
-          {children}
-        </ThemeProvider>
+        <ReactQueryProvider>
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
+            {children}
+          </ThemeProvider>
+        </ReactQueryProvider>
         <Toaster />
       </body>
     </html>
   );
 }
-
